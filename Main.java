d78c46130308a615d30387ed4eb2cb2690169f32
racheledler <55890230+racheledler@users.noreply.github.com>
2020-09-02 16:16:50 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+public static void main (String [] args){
+
+int age = 21;
+System.out.println("I am " + age + " years old.");
+
+String name = "Rachel";
+System.out.println("My name is " + name + ".");
+
+float price = 8.99f;
+System.out.println("My Chipotle will cost " + price +  "." );
+
+ boolean howardStudent = true;
+System.out.println ("Is it " + howardStudent + " that you are a Howard student?");
+
+
+}
+}
